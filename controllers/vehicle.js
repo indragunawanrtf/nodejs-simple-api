6125add43ce1397634c873b754ed96681e9550db
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-17 23:43:16 +0700

    Edit Position Coding

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -1,11 +1,24 @@
 const Vehicle = require("../models/vehicle.js");
 
 module.exports = app => {
+  
+  // Read API Vehicle
+  app.get("/api/vehicles", (req, res) => {
+    Vehicle.find()
+      .then(vehicles => {
+        res.json(vehicles);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 
-  // Create API
-  app.post('/api/vehicles', (req, res) => {
+  // Create API Vehicle
+  app.post("/api/vehicles", (req, res) => {
     const newVehicle = new Vehicle(req.body);
-    newVehicle.save().then(() => {
+    newVehicle
+      .save()
+      .then(() => {
         res.json({ Info: "Vehicles Created!" });
       })
       .catch(err => {
@@ -13,33 +26,25 @@ module.exports = app => {
       });
   });
 
-  // Read API
-  app.get('/api/vehicles', (req, res) => {
-    Vehicle.find().then(vehicles => {
-      res.json(vehicles)
-    }).catch(err => {
-      console.error(err)
-    });
-  });
-
-  // Update API
-  app.put('/api/vehicles/:id', (req, res) => {
+  // Update API Vehicle
+  app.put("/api/vehicles/:id", (req, res) => {
     Vehicle.update({ _id: req.params.id }, req.body)
-    .then(() => {
-      res.json({ info: "Vehicle Updated!"})
-    }).catch(err => {
-      console.error(err)
-    });
+      .then(() => {
+        res.json({ info: "Vehicle Updated!" });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   });
 
-  // Delete API
-  app.delete('/api/vehicles/:id', (req, res) => {
+  // Delete API Vehicle
+  app.delete("/api/vehicles/:id", (req, res) => {
     Vehicle.remove({ _id: req.params.id })
-    .then(() => {
-      res.json({ info: 'Vehicles Removed' })
-    }).catch(err => {
-      console.error(err)
-    })
-  })
-  
+      .then(() => {
+        res.json({ info: "Vehicles Removed" });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 };
